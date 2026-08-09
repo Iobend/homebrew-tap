c010f15e940c6ed0e8315970540d8caf20d4d35d
Tsaihemanth150 <tsaihemanth15@gmail.com>
2026-08-09 12:16:12 +0530

    feat: add Homebrew formula for iobend version 2.0.0

--- Formula/iobend.rb
+++ Formula/iobend.rb
@@ -1,24 +1,24 @@
 class Iobend < Formula
   desc "Developer environment manager for diagnostics, setup, and DevOps automation"
   homepage "https://github.com/Iobend/iobend"
-  version "2.0.0"
+  version "2.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Iobend/iobend/releases/download/v2.0.0/IOBend-v2.0.0-macos-arm64.tar.gz"
-      sha256 "8cc8425c2784980cbffd81c570c13165f0325e8d6c9956cda32bd5c197e1abb1"
+      url "https://github.com/Iobend/iobend/releases/download/v2.1.0/IOBend-v2.1.0-macos-arm64.tar.gz"
+      sha256 "489604efb712d8af5e38d0a66b9ce98314b6cae39fc7f2020728537fb694ced8"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Iobend/iobend/releases/download/v2.0.0/IOBend-v2.0.0-linux-arm64.tar.gz"
-      sha256 "1e6f34bb30ec89ff5815e1f6894e3e00367501e5ebc82ac1ef3f6aef40236e24"
+      url "https://github.com/Iobend/iobend/releases/download/v2.1.0/IOBend-v2.1.0-linux-arm64.tar.gz"
+      sha256 "0901d20d73965ee85ae3d77576f14307102a8ef6263abaa2c2578adabd3b7aa4"
     end
     on_intel do
-      url "https://github.com/Iobend/iobend/releases/download/v2.0.0/IOBend-v2.0.0-linux-x64.tar.gz"
-      sha256 "acaaa38b768aefd5429133b397892e2e08115b70c0239a4c68a9bc2f5dc4a7d8"
+      url "https://github.com/Iobend/iobend/releases/download/v2.1.0/IOBend-v2.1.0-linux-x64.tar.gz"
+      sha256 "f5f2c045713aa2f9a5f5927b037366ae8bc6cef0f84762fa5f4e535b675e5804"
     end
   end
 
